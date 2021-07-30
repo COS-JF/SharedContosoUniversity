41f4e7e2b2d80beff2c648efdd2b81a3c1941072
bcposit <bcposit1@gmail.com>
2021-07-30 11:30:49 -0500

    test commit 3

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -12,7 +12,7 @@ namespace ContosoUniversity
     public class Program
     {
         // Testing out some changes.
-        //*********steve o bug fix change**********
+        //*********steve o bug fix change  **********
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();

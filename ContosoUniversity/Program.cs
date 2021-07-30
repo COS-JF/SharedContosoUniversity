99a006a10102a448015b5055b4bfdb604e347ec1
COS-JF <Jeremiah.Fortner@shreveportla.gov>
2021-07-30 11:09:37 -0500

    Testing 3

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -17,7 +17,7 @@ namespace ContosoUniversity
             CreateHostBuilder(args).Build().Run();
         }
 
-        // Testing out some changes!
+        // Testing out some changes! 3
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>

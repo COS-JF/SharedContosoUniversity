c8aeac8aeb7b3a6c93acb9829d89b29c61b0908c
COS-JF <Jeremiah.Fortner@shreveportla.gov>
2021-07-30 09:57:05 -0500

    "Readding line in ErrorViewModel.cs"

--- ContosoUniversity/Models/ErrorViewModel.cs
+++ ContosoUniversity/Models/ErrorViewModel.cs
@@ -5,5 +5,7 @@ namespace ContosoUniversity.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
